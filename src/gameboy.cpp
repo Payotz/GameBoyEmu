40fb217f9b8bf075dc04cec48e4e5fa33bad03e7
Jack <josiah.cotas@gmail.com>
2017-09-26 13:53:53 -0700

    Started Foundation for CB Prefix Opcodes

--- src/gameboy.cpp
+++ src/gameboy.cpp
@@ -24,13 +24,15 @@ struct Register lh;
 unsigned short sp;
 unsigned short pc;
 
+bool interruptMode;
+
 
 void op_0x0(){
     pc +=2;
 } 
 
 void op_0x1(){
-    cb.whole = (opcode & 0x000F);
+    cb.whole = (opcode & 0x00FF);
     pc += 2;
 } 
 
@@ -75,6 +77,7 @@ void op_0x7(){
 }
 
 void op_0x8(){
+    sp = (opcode & 0x00FF);
     pc += 2;
 }
 
@@ -131,7 +134,7 @@ void op_1x0(){
 }
 
 void op_1x1(){
-    ed.whole = (opcode & 0x000F);
+    ed.whole = (opcode & 0x00FF);
     pc += 2;
 }
 
@@ -167,7 +170,7 @@ void op_1x5(){
 }
 
 void op_1x6(){  // LD nn.n = Put value nn into n | D,n
-    ed.high = (opcode & 0x000F);
+    ed.low = (opcode & 0x000F);
     pc += 2;
 }
 
@@ -176,7 +179,7 @@ void op_1x7(){
 }
 
 void op_1x8(){
-    pc += ((opcode & 0x000F));
+    pc += (opcode & 0x000F);
 }
 
 void op_1x9(){
@@ -219,7 +222,7 @@ void op_1xD(){
 }
 
 void op_1xE(){
-    ed.low = ed.high;
+    ed.low = opcode & 0x000F;
     pc +=2;
 }
 
@@ -459,7 +462,7 @@ void op_3xD(){
 }
 
 void op_3xE(){
-    ed.low = fa.low;
+    fa.low = opcode & 0x000F;
     pc +=2;
 }
 
@@ -777,7 +780,6 @@ void op_7x5(){
 }
 
 void op_7x6(){
-    lh.whole = (opcode & 0x000F);
     pc += 2;
 }
 
@@ -1786,6 +1788,11 @@ void op_DxD(){
 }
 
 void op_DxE(){
+    fa.low -= ed.high;
+    if(fa.low == 0){
+        flagreg_z = 0;   
+    }
+    flagreg_n = 1;
     pc += 2;
 }
 
@@ -1806,6 +1813,7 @@ void op_Ex1(){
 }
 
 void op_Ex2(){
+    fa.low = cb.high; 
     pc += 2;
 }
 
@@ -1823,6 +1831,7 @@ void op_Ex5(){
 }
 
 void op_Ex6(){
+    fa.low = fa.low & (opcode & 0x000F);
     pc += 2;
 }
 
@@ -1842,10 +1851,12 @@ void op_Ex9(){
 }
 
 void op_ExA(){
+    fa.low == (opcode & 0x00FF);
     pc += 2;
 }
 
 void op_ExB(){
+    
     pc += 2;
 }
 
@@ -1858,6 +1869,7 @@ void op_ExD(){
 }
 
 void op_ExE(){
+    fa.low ^= (opcode & 0x000F);
     pc += 2;
 }
 
@@ -1868,6 +1880,7 @@ void op_ExF(){
 }
 
 void op_Fx0(){
+    
     pc += 2;
 }
 
@@ -1877,14 +1890,17 @@ void op_Fx1(){
 }
 
 void op_Fx2(){
+    
     pc += 2;
 }
 
 void op_Fx3(){
+    interruptMode = false;
     pc += 2;
 }
 
 void op_Fx4(){
+    
     pc += 2;
 }
 
@@ -1895,6 +1911,7 @@ void op_Fx5(){
 }
 
 void op_Fx6(){
+    fa.low |= (opcode & 0x000F);
     pc += 2;
 }
 
@@ -1920,6 +1937,7 @@ void op_FxA(){
 }
 
 void op_FxB(){
+    interruptMode = true;
     pc += 2;
 }
 
@@ -1932,6 +1950,12 @@ void op_FxD(){
 }
 
 void op_FxE(){
+    auto result = fa.low - (opcode & 0x000F);
+    if(result == 0){
+        flagreg_z = 1;
+    }
+    
+    flagreg_n = 1;
     pc += 2;
 }
 
@@ -1940,6 +1964,394 @@ void op_FxF(){
     sp++;
     pc = 0x0038;
 }
+
+void op_CB_0x1(){
+}
+
+void op_CB_0x2(){   
+}
+
+void op_CB_0x3(){ 
+}
+
+void op_CB_0x4(){
+}
+
+void op_CB_0x5(){
+}
+
+void op_CB_0x6(){
+}
+
+void op_CB_0x7(){ 
+}
+
+void op_CB_0x8(){
+
+}
+
+void op_CB_0x9(){
+}
+
+void op_CB_0xA(){
+    
+}
+
+void op_CB_0xB(){
+    
+}
+
+void op_CB_0xC(){
+}
+
+void op_CB_0xD(){
+}
+
+void op_CB_0xE(){
+}
+
+void op_CB_0xF(){
+}
+
+void op_CB_1x0(){
+}
+
+void op_CB_1x1(){
+}
+
+void op_CB_1x2(){
+}
+
+void op_CB_1x3(){
+}
+
+void op_CB_1x4(){   
+}
+
+void op_CB_1x5(){
+}
+
+void op_CB_1x6(){
+}
+
+void op_CB_1x7(){
+}
+
+void op_CB_1x8(){
+}
+
+void op_CB_1x9(){
+}
+
+void op_CB_1xA(){
+}
+
+void op_CB_1xB(){
+}
+
+void op_CB_1xC(){
+}
+
+void op_CB_1xD(){
+}
+
+void op_CB_1xE(){
+}
+
+void op_CB_1xF(){
+}
+
+void op_CB_2x0(){
+}
+
+void op_CB_2x1(){
+}
+
+void op_CB_2x2(){
+}
+
+void op_CB_2x3(){
+}
+
+void op_CB_2x4(){
+}
+
+void op_CB_2x5(){
+}
+
+void op_CB_2x6(){
+}
+
+void op_CB_2x7(){
+}
+
+void op_CB_2x8(){
+}
+
+void op_CB_2x9(){
+}
+
+void op_CB_2xA(){
+}
+
+void op_CB_2xB(){
+}
+
+void op_CB_2xC(){
+}
+
+void op_CB_2xD(){
+}
+
+void op_CB_2xE(){
+}
+
+void op_CB_2xF(){
+}
+
+void op_CB_3x0(){
+}
+
+void op_CB_3x1(){
+}
+
+void op_CB_3x2(){
+}
+
+void op_CB_3x3(){
+}
+
+void op_CB_3x4(){
+}
+
+void op_CB_3x5(){
+}
+
+void op_CB_3x6(){
+}
+void op_CB_3x7(){
+}
+void op_CB_3x8(){
+}
+void op_CB_3x9(){
+}
+void op_CB_3xA(){
+}
+void op_CB_3xB(){
+}
+void op_CB_3xC(){
+}
+void op_CB_3xD(){
+}
+void op_CB_3xE(){
+}
+void op_CB_3xF(){
+}
+void op_CB_4x0(){
+}
+void op_CB_4x1(){
+}
+void op_CB_4x2(){
+}
+void op_CB_4x3(){
+}
+void op_CB_4x4(){
+}
+void op_CB_4x5(){
+}
+void op_CB_4x6(){
+}
+void op_CB_4x7(){
+}
+void op_CB_4x8(){
+}
+void op_CB_4x9(){
+}
+void op_CB_4xA(){
+}
+void op_CB_4xB(){
+}
+void op_CB_4xC(){
+}
+void op_CB_4xD(){
+}
+void op_CB_4xE(){
+}
+void op_CB_4xF(){
+}
+void op_CB_5x0(){
+}
+void op_CB_5x1(){
+}
+void op_CB_5x2(){
+}
+void op_CB_5x3(){
+}
+void op_CB_5x4(){
+}
+void op_CB_5x5(){
+}
+void op_CB_5x6(){
+}
+void op_CB_5x7(){
+}
+void op_CB_5x8(){
+}
+void op_CB_5x9(){
+}
+void op_CB_5xA(){
+}
+void op_CB_5xB(){
+}
+void op_CB_5xC(){
+}
+void op_CB_5xD(){
+}
+void op_CB_5xE(){
+}
+void op_CB_5xF(){
+}
+void op_CB_6xA(){
+}
+void op_CB_6xB(){
+}
+void op_CB_6xC(){
+}
+void op_CB_6xD(){
+}
+void op_CB_6xE(){
+}
+void op_CB_6xF(){
+}
+void op_CB_7x0(){
+}
+void op_CB_7x1(){
+}
+void op_CB_7x2(){
+}
+void op_CB_7x3(){
+}
+void op_CB_7x4(){
+}
+void op_CB_7x5(){
+}
+void op_CB_7x6(){
+}
+void op_CB_7x7(){
+}
+void op_CB_7x8(){
+}
+void op_CB_7x9(){
+}
+void op_CB_7xA(){
+}
+void op_CB_7xB(){
+}
+void op_CB_7xC(){
+}
+void op_CB_7xD(){
+}
+void op_CB_7xE(){
+}
+void op_CB_7xF(){
+}
+void op_CB_8x0(){
+}
+void op_CB_8x1(){
+}
+void op_CB_8x2(){
+}
+void op_CB_8x3(){
+}
+void op_CB_8x4(){
+}
+void op_CB_8x5(){
+}
+void op_CB_8x6(){
+}
+void op_CB_8x7(){
+}
+void op_CB_8x8(){
+}
+void op_CB_8x9(){
+}
+void op_CB_8xA(){
+}
+void op_CB_8xB(){
+}
+void op_CB_8xC(){
+}
+void op_CB_8xD(){
+}
+void op_CB_8xE(){
+}
+void op_CB_8xF(){
+}
+void op_CB_9x0(){
+}
+void op_CB_9x1(){
+}
+void op_CB_9x2(){
+}
+void op_CB_9x3(){
+}
+void op_CB_9x4(){
+}
+void op_CB_9x5(){
+}
+void op_CB_9x6(){
+}
+void op_CB_9x7(){
+}
+void op_CB_9x8(){
+}
+void op_CB_9x9(){
+}
+void op_CB_9xA(){
+}
+void op_CB_9xB(){
+}
+void op_CB_9xC(){
+}
+void op_CB_9xD(){
+}
+void op_CB_9xE(){
+}
+void op_CB_9xF(){
+}
+void op_CB_Ax0(){
+}
+void op_CB_Ax1(){
+}
+void op_CB_Ax2(){
+}
+void op_CB_Ax3(){
+}
+void op_CB_Ax4(){
+}
+void op_CB_Ax5(){
+}
+void op_CB_Ax6(){
+}
+void op_CB_Ax7(){
+}
+void op_CB_Ax8(){
+}
+void op_CB_Ax9(){
+}
+void op_CB_AxA(){
+}
+void op_CB_AxB(){
+}
+void op_CB_AxC(){
+}
+void op_CB_AxD(){
+}
+void op_CB_AxE(){
+}
+void op_CB_AxF(){
+}
+
 
 void GameBoy::initialize(){
     isRunning = true;
@@ -1958,6 +2370,7 @@ void GameBoy::initialize(){
     }
     pc = 0x100;
     sp = 0;
+    interruptMode = false;
 }
 
 void GameBoy::loadFile(const char* fileName){
